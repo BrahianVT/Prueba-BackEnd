62f5ef52056431dbb63133c3b5bca9371fb7686b
BrahianVT <pumasemj@hotmail.com>
2020-11-27 23:57:55 -0600

    Se agrega clase principal

--- resuelveExercise/src/main/java/edu/exercise/resuelve/Main.java
+++ resuelveExercise/src/main/java/edu/exercise/resuelve/Main.java
@@ -1,4 +1,22 @@
 package edu.exercise.resuelve;
 
+import java.util.List;
+
 public class Main {
+
+    public  static void main(String[] args) {
+        Main main = new Main();
+
+        JsonUtilities cargar = new JsonUtilities();
+        List<Jugador> jugadores = cargar.cargarArchivoJson("input.json");
+
+        System.out.println("ARCHIVO CARGADO CORRECTAMENTE PROCESANDO ...");
+        CalcularSueldo calcularSueldo = new CalcularSueldo();
+        calcularSueldo.llenarListaNiveles();
+        calcularSueldo.calcularGoles(jugadores);
+        calcularSueldo.calcularSalario(jugadores);
+        cargar.guardarArchivoJsonSalida("output.json", jugadores);
+
+        System.out.println("ARCHIVO GUARDADO CORRECTAMENTE PROCESANDO ...");
+    }
 }
