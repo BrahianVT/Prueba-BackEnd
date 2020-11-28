27da67b4747b1ff65f4e7290cbac67dce9528db1
BrahianVT <pumasemj@hotmail.com>
2020-11-28 11:52:39 -0600

    Se agrega funcionalidad para calcular los sueldos basado en los equipos que se reciben de la entrada

--- resuelveExercise/src/main/java/edu/exercise/resuelve/CalcularSueldo.java
+++ resuelveExercise/src/main/java/edu/exercise/resuelve/CalcularSueldo.java
@@ -1,31 +1,35 @@
 package edu.exercise.resuelve;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class CalcularSueldo {
-    private float totalGolesMesNecesarios;
-    private float totalGolesPorMes;
-    private float porcentajeBonoEquipo;
+    private Map<String, Float> totalGolesMesNecesarios = new HashMap<>();
+    private Map<String, Float> totalGolesPorMes = new HashMap<>();
+    private Map<String, Float> porcentajeBonoEquipo = new HashMap<>();
     private List<Nivel> listaNiveles = new ArrayList<Nivel>();
 
-
-
     public void calcularSalario(List<Jugador> jugadores){
-        calcularPorcetajesBonoEquipo();
+        calcularPorcetajesBonoEquipo(jugadores);
         for(Jugador jugador: jugadores){
             float bonoBase = jugador.getBono() /2;
-            float bonoReal = (bonoBase * calcularPorcentajeBonoJugador(jugador) / 100) + (bonoBase * porcentajeBonoEquipo /100);
+            float bonoReal = (bonoBase * calcularPorcentajeBonoJugador(jugador) / 100) + (bonoBase * porcentajeBonoEquipo.get(jugador.getEquipo()) /100);
             jugador.setBonoReal(bonoReal);
             jugador.setSueldoCompleto(bonoReal + jugador.getSueldo());
         }
     }
-    public void calcularPorcetajesBonoEquipo(){
-        if(totalGolesPorMes >= totalGolesMesNecesarios)
-            porcentajeBonoEquipo = 100.f;
-        else
-            porcentajeBonoEquipo = (totalGolesPorMes * 100) / totalGolesMesNecesarios;
+    public void calcularPorcetajesBonoEquipo(List<Jugador> jugadores){
+        calcularGoles(jugadores);
 
+        for(Map.Entry<String, Float> equipo : totalGolesPorMes.entrySet()){
+            if(equipo.getValue() >= totalGolesMesNecesarios.get(equipo.getKey()))
+                porcentajeBonoEquipo.put(equipo.getKey(), 100f);
+            else {
+                porcentajeBonoEquipo.put(equipo.getKey(), equipo.getValue() * 100 / totalGolesMesNecesarios.get(equipo.getKey()));
+            }
+        }
     }
     public float calcularPorcentajeBonoJugador(Jugador jugador){
         int golesJugador = jugador.getGoles();
@@ -37,8 +41,11 @@ public class CalcularSueldo {
     }
     public void calcularGoles(List<Jugador> jugadores){
         for(Jugador jugador: jugadores){
-            totalGolesMesNecesarios+= obtenerGolesPorNivel(jugador.getNivel());
-            totalGolesPorMes+= jugador.getGoles();
+           float golesNecesariosPorEquipo = totalGolesMesNecesarios.getOrDefault(jugador.getEquipo(), 0.0f) +
+                   obtenerGolesPorNivel(jugador.getNivel());
+           float golesPorEquipo = totalGolesPorMes.getOrDefault(jugador.getEquipo(), 0.0f) + jugador.getGoles();
+           totalGolesMesNecesarios.put(jugador.getEquipo(), golesNecesariosPorEquipo);
+           totalGolesPorMes.put(jugador.getEquipo(), golesPorEquipo);
         }
     }
     public int obtenerGolesPorNivel(String nivel){
@@ -54,28 +61,27 @@ public class CalcularSueldo {
         listaNiveles.add(new Nivel("Cuauh", 20));
     }
 
-
-    public float getTotalGolesMesNecesarios() {
+    public Map<String, Float> getTotalGolesMesNecesarios() {
         return totalGolesMesNecesarios;
     }
 
-    public void setTotalGolesMesNecesarios(float totalGolesMesNecesarios) {
+    public void setTotalGolesMesNecesarios(Map<String, Float> totalGolesMesNecesarios) {
         this.totalGolesMesNecesarios = totalGolesMesNecesarios;
     }
 
-    public float getTotalGolesPorMes() {
+    public Map<String, Float> getTotalGolesPorMes() {
         return totalGolesPorMes;
     }
 
-    public void setTotalGolesPorMes(float totalGolesPorMes) {
+    public void setTotalGolesPorMes(Map<String, Float> totalGolesPorMes) {
         this.totalGolesPorMes = totalGolesPorMes;
     }
 
-    public float getPorcentajeBonoEquipo() {
+    public Map<String, Float> getPorcentajeBonoEquipo() {
         return porcentajeBonoEquipo;
     }
 
-    public void setPorcentajeBonoEquipo(float porcentajeBonoEquipo) {
+    public void setPorcentajeBonoEquipo(Map<String, Float> porcentajeBonoEquipo) {
         this.porcentajeBonoEquipo = porcentajeBonoEquipo;
     }
 
