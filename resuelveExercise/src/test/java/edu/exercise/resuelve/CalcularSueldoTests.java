f7ada9df201c6f90d14b3325bd7319ccfe02581a
BrahianVT <pumasemj@hotmail.com>
2021-03-05 23:05:08 -0600

    Se resuelve error dado que no ejecutava las pruebas unitarias

--- resuelveExercise/src/test/java/edu/exercise/resuelve/CalcularSueldoTests.java
+++ resuelveExercise/src/test/java/edu/exercise/resuelve/CalcularSueldoTests.java
@@ -4,6 +4,10 @@ import edu.exercise.resuelve.Services.CalcularSueldo;
 import edu.exercise.resuelve.entities.Jugador;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,7 +18,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  * Pruebas unitarias para el calculo de los salarios
  * */
 
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
 public class CalcularSueldoTests {
+
     CalcularSueldo calcularSueldo;
 
     @BeforeEach
